d39421da11eb633a4656f5f323bf6eafb0572edc
ForgottenPluto <pluto-ljx@qq.com>
2017-03-29 10:15:26 +0800

    170329a

--- ife-1-9/assets/js/main.js
+++ ife-1-9/assets/js/main.js
@@ -1,3 +1,65 @@
-/**
- * Created by liangjinxie on 2017/3/16.
- */
+window.onload = function () {
+    $(".publicButton a").click(buttonOnClickFile());
+    $(".publicVice-Button a").click(buttonOnClickDocument());
+
+    function buttonOnClickFile() {
+        var $folder = $(this).parent().siblings();
+        if ($folder.is(":visible")) {
+            $folder.hide()
+                .siblings().removeClass("publicButton-On")
+                .children().attr("src", "assets/imags/demo2.png")
+        } else {
+            $folder.show()
+                .siblings().addClass("publicButton-On")
+                .children().attr("src", "assets/imags/demo6.png");
+        }
+    }
+
+    function buttonOnClickDocument() {
+        var $folder = $(this).parent().siblings();
+        if ($folder.is(":visible")) {
+            $folder.hide()
+                .siblings().removeClass("publicButton-On")
+                .children().attr("src", "assets/imags/demo7.png")
+        } else {
+            $folder.show()
+                .siblings().addClass("publicButton-On")
+                .children().attr("src", "assets/imags/demo6.png");
+        }
+    }
+
+//
+//
+//
+//     
+// 
+// 
+// 
+// 
+// $(".publicButton a").click(function () {
+//         var $folder = $(this).parent().siblings();
+//         if ($folder.is(":visible")) {
+//             $folder.hide()
+//                 .siblings().removeClass("publicButton-On")
+//                     .children().attr("src","assets/imags/demo2.png")
+//
+//
+//
+//
+//                 // .children().removeClass("publicVice-on-Button")
+//                 // .addClass("publicVice-Button");
+//
+//         } else {
+//             $folder.show()
+//                 .siblings().addClass("publicButton-On")
+//                 .children().attr("src","assets/imags/demo6.png");
+//
+//
+//                 // .children().removeClass("publicVice-Button")
+//                 // .addClass("publicVice-on-Button");
+//
+//         }
+//
+//     });
+//
+};
