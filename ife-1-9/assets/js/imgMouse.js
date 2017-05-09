86720378efcad62e14bf3726d1f8b95877a90ac4
forgottenPluto <pluto-ljx@qq.com>
2017-05-10 00:15:45 +0800

    170510a

--- ife-1-9/assets/js/imgMouse.js
+++ ife-1-9/assets/js/imgMouse.js
@@ -1,3 +1,46 @@
 /**
  * Created by Administrator on 2017/5/4.
  */
+$(function () {
+    function imgHtml() {
+        var $img = $(this).find("img");
+        var img_w = $img.width();
+        var ico_marginLeft = img_w * 0.5 - 30;
+        var img_h = $img.height();
+        var spanHtml =
+            '<span class="imgeMask" style="position: absolute; top:5px;left:20px; width:' +
+            img_w +
+            'px;height:' +
+            img_h +
+            'px;"><div style="display: none;margin-top: 0%; margin-left:' +
+            ico_marginLeft +
+            'px;width:' +
+            img_w +
+            '"><a class="BlowUp" href="#" title="Blow Up"><i class="iconfont">&#xe6d3;</i></a><a class="Download" href="#" title="Download"><i class="iconfont">&#xe6c5;</i></a><a class="Delete" href="#" title="Delete"><i class="iconfont">&#xe649;</i></a></div></span>';
+        $(this).append(spanHtml);
+    }
+
+    $(".depotWrapper").each(imgHtml);
+    $(".depotWrapper").on("mouseenter mouseleave", ".imgeMask", function (index) {
+        var $imgOverBlack = $(this).children();
+        var $img = $(this).siblings().find("img");
+        $(this).toggleClass("imgeOver");
+        if ($imgOverBlack.hasClass("icoOver")) {
+            $imgOverBlack.eq(0).removeClass("icoOver")
+                .stop(true, false)
+                .animate({marginTop: "0"}, 0);
+            return false;
+        } else {
+            var imgNewWitth = $img.width();
+            var imgNewHeight = $img.height();
+            var icoNewMar = imgNewWitth * 0.5 - 30;
+            $(this).width(imgNewWitth);
+            $(this).height(imgNewHeight);
+            $imgOverBlack.css("margin-left", icoNewMar);
+            $imgOverBlack.eq(0).addClass("icoOver")
+                .stop(true, false)
+                .animate({marginTop: "28%"}, 150);
+        }
+    })
+
+});
---
+++ ife-1-9/assets/js/promptLable.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+;(function($,window,document,undefined){
+
+
+
+
+
+
+
+})(jQuery,window,document);
